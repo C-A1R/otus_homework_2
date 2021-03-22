0753257bbb690e8866e99779ab571bb2d409854b
CorsAIR <al.korsakov@list.ru>
2021-03-22 16:06:41 +0300

    start

---
+++ Container.cpp
@@ -0,0 +1,9 @@
+#include "Container.h"
+
+// #include <cstdlib>
+#include <iostream>
+
+Container::Container()
+{
+    std::cout << "# container" << std::endl;
+}
---
+++ main.cpp
@@ -0,0 +1,12 @@
+
+#include "Container.h"
+
+#include <iostream>
+
+int main (int, char **)
+{
+    std::cout << "### allocator START" << std::endl;
+    auto c = Container();
+
+    return 0;
+}
